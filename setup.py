6e826fa14c5e630f5e36eadf8bd2ae498dbcbb3f
anderson949 <138420698+anderson949@users.noreply.github.com>
2024-11-25 08:21:43 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     name="iqoptionapi",
     version=api_version,
     packages=find_packages(),
-    install_requires=["pylint", "requests", "websocket-client==0.56"],
+    install_requires=["pylint", "requests", "websocket-client==1.8.0"],
     include_package_data=True,
     description="Best IQ Option API for python",
     long_description="Best IQ Option API for python",
-    url="https://github.com/iqoptionapi/iqoptionapi",
-    author="Rafael Faria",
+    url="https://github.com/anderson949/iqoptionapi-1",
+    author="@stealthlord_anonymous",
     zip_safe=False
 )
